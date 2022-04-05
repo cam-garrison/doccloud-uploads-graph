b5fe9a354b72e78fa9706ae19dc73a9c99730873
Cameron Garrison <cgarriso@bu.edu>
2022-04-05 18:07:17 -0400

    update graphing and reqs

--- main.py
+++ main.py
@@ -4,9 +4,18 @@ upload frequency from a given user.
 """
 
 from documentcloud.addon import AddOn
-
+import pandas as pd
 
 class UploadGraph(AddOn):
+
+    def create_df(self, doc_dates):
+        df = pd.DataFrame(doc_dates, columns=["datetime"])
+        df["datetime"] = df["datetime"].astype("datetime64")
+        df['date'] = df['datetime'].dt.date
+        df = df.sort_values(by='date')
+        df.insert(0, 'count', range(1,1 + len(df)))
+        return df    
+
 
     def main(self):
         # fetch your add-on specific data
@@ -21,14 +30,19 @@ class UploadGraph(AddOn):
         for document in documents:
             document_dates.append(document.created_at)
 
+        df = self.create_df(doc_dates=document_dates)
+        lines = df.plot(y="count", kind='line', x="date",
+                title= username + ": Uploads Over Time",
+                figsize=(12,8))
         
+        fig = lines.get_figure()
+        fig.savefig('useruploads.png')
 
-        with open("hello.txt", "w+") as file_:
-            file_.write("Hello world!")
+
+        with open('useruploads.png', "rb") as file_:
             self.upload_file(file_)
 
-        self.set_message("Hello World end!")
-        self.send_mail("Hello World!", "We finished!")
+        self.set_message("Uploads Graph end!")
 
 
 if __name__ == "__main__":
